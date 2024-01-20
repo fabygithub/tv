c43369f1e8386e692b2658a78ec9229d24434edb
fabian cornejo <36684138+fabygithub@users.noreply.github.com>
2024-01-20 20:22:29 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 function a() {
 var iframe = document.getElementById("iframe");
 iframe.src = "https://rudo.video/live/tvu"
@@ -8,4 +9,49 @@ function b() {
 var iframe = document.getElementById("iframe");
 iframe.src = "https://rudo.video/live/ucvtv2"
 }
+
+
+function windowClose() {
+window.open('','_parent','');
+window.close();
+}
+
+function borrar() {
+this.parentNode.parentNode.remove(); return false;
+}
+*/
+
+
+/*
+function b(){
+	 var wdw = window.open("url-or-blank", ""); wdw.document.write("<iframe id='iframeID' class='external' src='https://rudo.video/live/ucvtv2' name='auth' scrolling='no' sandbox='allow-forms allow-orientation-lock allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-presentation allow-scripts allow-top-navigation allow-top-navigation-by-user-activation'></iframe> ");
+}
+*/
+
+function a(){
+	 window.open("https://rudo.video/live/tvu", "iframe");
+}
+
+
+function b(){
+	 window.open("https://rudo.video/live/ucvtv2", "iframe");
+}
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
 
