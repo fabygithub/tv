578f240b1b39f3f093ea763d8c6d0bf06df28f92
fabian cornejo <36684138+fabygithub@users.noreply.github.com>
2024-01-20 13:14:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ var iframe = document.getElementById("iframe");
 iframe.src = "https://rudo.video/live/tvu"
 }
 
+
+function b() {
+var iframe = document.getElementById("iframe");
+iframe.src = "https://rudo.video/live/ucvtv2"
+}
+
