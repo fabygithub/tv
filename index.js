d3447a20a48b1f6e1550f1f843f3b644bbd357b0
fabian cornejo <36684138+fabygithub@users.noreply.github.com>
2024-01-23 18:36:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
+/*
+function a() {
+var iframe = document.getElementById("iframe");
+iframe.src = "https://rudo.video/live/tvu"
+}
+
+
+function b() {
+var iframe = document.getElementById("iframe");
+iframe.src = "https://rudo.video/live/ucvtv2"
+}
+
+
+function windowClose() {
+window.open('','_parent','');
+window.close();
+}
+
+function borrar() {
+this.parentNode.parentNode.remove(); return false;
+}
+*/
+
+
+/*
+function b(){
+	 var wdw = window.open("url-or-blank", ""); wdw.document.write("<iframe id='iframeID' class='external' src='https://rudo.video/live/ucvtv2' name='auth' scrolling='no' sandbox='allow-forms allow-orientation-lock allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-presentation allow-scripts allow-top-navigation allow-top-navigation-by-user-activation'></iframe> ");
+}
+*/
+
+
 function a(){
 	 window.open("https://rudo.video/live/tvu", "iframe");
 }
@@ -6,6 +37,40 @@ function a(){
 function b(){
 	 window.open("https://rudo.video/live/ucvtv2", "iframe");
 }
+
+
+function c(){
+	 window.open("https://mdstrm.com/live-stream/5c0e8b19e4c87f3f2d3e6a59?jsapi=true&amp;loop=false&amp;autoplay=true&amp;controls=true&amp;player=5c0e87c403a35601ebcffcf4", "iframe");
+}
+
+function d(){
+	 window.open("https://rudo.video/live/c13?c3=13CL&amp;autostart=1&amp;volume=0&amp;tag=eyJwcmUiOiJodHRwczpcL1wvcHViYWRzLmcuZG91YmxlY2xpY2submV0XC9nYW1wYWRcL2Fkcz9pdT1cLzExMjM3MjIwN1wvZW52aXZvLmNsXC9lbnZpdm9cL3ByZXJvbGwmZGVzY3JpcHRpb25fdXJsPWh0dHAlM0ElMkYlMkZ3d3cuMTMuY2wlMkZlbi12aXZvJmVudj12cCZpbXBsPXMmY29ycmVsYXRvcj0mdGZjZD0wJm5wYT0wJmdkZnBfcmVxPTEmb3V0cHV0PXZhc3Qmc3o9NjU2eDM2OCZjaXVfc3pzPTcyOHg5MCw5NzB4OTAsOTcweDI1MCwxOTAweDgyNSwxOTIweDc2OCw0MDB4NjAmdW52aWV3ZWRfcG9zaXRpb25fc3RhcnQ9MSZwbW5kPTAmcG14ZD0xMjAwMDAmcG1hZD0yJnBvZD0xIiwicG9zdCI6IjAiLCJtaWQiOnsidGFnIjoiMCIsInRpbWUiOltdfSwib3ZlciI6eyJ0YWciOiJodHRwczpcL1wvcHViYWRzLmcuZG91YmxlY2xpY2submV0XC9nYW1wYWRcL2Fkcz9pdT1cLzExMjM3MjIwN1wvZW52aXZvLmNsXC9lbnZpdm9cL292ZXJsYXkmZGVzY3JpcHRpb25fdXJsPWh0dHAlM0ElMkYlMkZ3d3cuMTMuY2wlMkZlbi12aXZvJmVudj12cCZpbXBsPXMmY29ycmVsYXRvcj0mdGZjZD0wJm5wYT0wJmdkZnBfcmVxPTEmb3V0cHV0PXZhc3QmdmFkX3R5cGU9bm9ubGluZWFyJnN6PTQwMHg2MCZtaSIsInRpbWUiOls2MF19fQ==", "iframe");
+}
+
+function e(){
+	 window.open("https://www.youtube.com/embed/cb12KmMMDJA?feature=oembed&amp;autoplay=1&amp", "iframe");
+}
+
+function f(){
+	 window.open("https://www.youtube.com/embed/LY2XEQ_SSXA?autoplay=true", "iframe");
+}
+
+
+
+
+
+/*
+https://mdstrm.com/live-stream/53d2c1a32640614e62a0e000?jsapi=true&amp;loop=false&amp;autoplay=true&amp;volume=0&amp;player=5e5597b9171de20f8f30667a&amp;access_token=GM81JL9uaIX2sgPLeo6bdHzlw4oEH4gBsSGbBUNeGJC8gqUrccji3Dd77gNZKsCMjYh4GQKnPg7&amp;mse=true&amp;ads[map]=&amp;custom.dfp=%2Fmega.cl%2Fsenal-en-vivo%2Fhome%2F&amp;custom.kv=seccion%253Dotros%2526nivel%253Dhome%2526nota%253D%2526id_nota%253D%2526tipo%253Dotro%2526sev_companion%253Dsev_companion&amp;custom.desc_url=https%3A%2F%2Fwww.mega.cl%2Fsenal-en-vivo%2F
+*/
+
+
+/*
+"https://mdstrm.com/live-stream/5c0e8b19e4c87f3f2d3e6a59?jsapi=true&amp;loop=false&amp;autoplay=true&amp;controls=true&amp;player=5c0e87c403a35601ebcffcf4"
+*/
+
+ 
+
+
 
 
 
