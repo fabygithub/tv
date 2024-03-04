0be9c56e25f9d3cdd14ee53b6a8d73da5b0c0911
fabian cornejo <36684138+fabygithub@users.noreply.github.com>
2024-03-03 21:48:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -128,7 +128,12 @@ function a10(){
 	 window.open("http://mdstrm.com/live-stream/57b4dbf5dbbfc8f16bb63ce1", "iframe");
 }
 
-	
+
+function a11(){
+	 window.open("https://mdstrm.com/live-stream/603d4e1fb042ce07c5c8f911?autoplay=true&player=603d50916a45d707d3889300", "iframe");
+}
+
+
 
 /*
 https://mdstrm.com/live-stream/53d2c1a32640614e62a0e000?jsapi=true&amp;loop=false&amp;autoplay=true&amp;volume=0&amp;player=5e5597b9171de20f8f30667a&amp;access_token=GM81JL9uaIX2sgPLeo6bdHzlw4oEH4gBsSGbBUNeGJC8gqUrccji3Dd77gNZKsCMjYh4GQKnPg7&amp;mse=true&amp;ads[map]=&amp;custom.dfp=%2Fmega.cl%2Fsenal-en-vivo%2Fhome%2F&amp;custom.kv=seccion%253Dotros%2526nivel%253Dhome%2526nota%253D%2526id_nota%253D%2526tipo%253Dotro%2526sev_companion%253Dsev_companion&amp;custom.desc_url=https%3A%2F%2Fwww.mega.cl%2Fsenal-en-vivo%2F
